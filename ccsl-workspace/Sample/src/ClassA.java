48585494d992d749a93c13545beae6d1d3604ed0
elderjr <elderjr1995@gmail.com>
2020-06-30 19:38:14 -0300

    BinaryExpression added in metaclass

--- ccsl-workspace/Sample/src/ClassA.java
+++ ccsl-workspace/Sample/src/ClassA.java
@@ -5,12 +5,15 @@ public abstract class ClassA {
 	public static int a;
 	
 	
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		new ClassA() {
 			
 		};
 		main(new String[] {"a"});
-		
+		int a = ~3;
+		int b = 2;
+		int c = a >> b;
+	
 	
 	}
 }
